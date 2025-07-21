8eaa7a08928ca31344ebf945b31c9f16f6bf5711
Rangel Reale <rangelspam@gmail.com>
2025-07-21 16:25:39 -0300

    example

---
+++ example_test.go
@@ -0,0 +1,58 @@
+package nbjobqueue_test
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"sync"
+
+	"github.com/rrgmc/nbjobqueue"
+)
+
+func ExampleQueue() {
+	jq := nbjobqueue.New(4) // start a job queue with 4 goroutines.
+
+	var items []int
+	var lock sync.Mutex
+
+	// Add 10 jobs. At most, 4 jobs will run at the same time.
+	// AddJob NEVER blocks.
+	for i := 0; i < 10; i++ {
+		jq.AddJob(func() {
+			lock.Lock()
+			defer lock.Unlock()
+			items = append(items, i)
+		})
+	}
+
+	jq.Close() // wait for all jobs to be done, then release all resources.
+
+	slices.Sort(items)
+	fmt.Println(items)
+
+	// Output: [0 1 2 3 4 5 6 7 8 9]
+}
+
+func ExampleQueueCtx() {
+	jq := nbjobqueue.NewWithContext(context.Background(), 4) // start a job queue with 4 goroutines.
+
+	var items []int
+	var lock sync.Mutex
+
+	// Add 10 jobs. At most, 4 jobs will run at the same time.
+	// AddJob NEVER blocks.
+	for i := 0; i < 10; i++ {
+		jq.AddJob(func(ctx context.Context) {
+			lock.Lock()
+			defer lock.Unlock()
+			items = append(items, i)
+		})
+	}
+
+	jq.CancelAndClose() // cancels the context sent to jobs, and close without processing any remaining jobs.
+
+	slices.Sort(items)
+	fmt.Println(items)
+
+	// Output: [0 1 2 3 4 5 6 7 8 9]
+}
